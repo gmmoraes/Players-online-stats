1b46c3f6c652588da29b92e7cda03b43ec8a0af0
gmmoraes <gabrielgmmoraes@gmail.com>
2016-07-16 17:36:15 -0300

    Update statstibia.py

--- statstibia.py
+++ statstibia.py
@@ -1,29 +1,35 @@
-from bs4 import BeautifulSoup
-import urllib.request
 import sqlite3
-from matplotlib import pyplot as plt
-from cairocffi import *
-from tkinter import*
 import time
 import datetime
 import numpy as np
+import urllib.request
+from bs4 import BeautifulSoup
+from matplotlib import pyplot as plt
 from matplotlib.dates import AutoDateLocator, AutoDateFormatter, date2num
+from tkinter import*
 plt.style.use('ggplot')
 
 
 
-class Stats():
+class DataStore():
     def __init__(self):
         self.conn = sqlite3.connect('test2.db')
-        self.c = self.conn.cursor()
-        self.create_table()
-        self.a = []
-        self.dates = datetime.datetime.now().date()
+        self._create_table()
+        self.get_all_players()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+
+
+    def get_all_players(self):
         url = "https://secure.tibia.com/community/?subtopic=worlds"
         page = urllib.request.urlopen(url)
         soup = BeautifulSoup(page.read(), 'html.parser')
         stats = soup.find_all("div", {"id":"RightArtwork"})
         number = soup.find_all("tr", {"class":"Odd"})
+        dates = datetime.datetime.now().date()
 
         for totalOnline in stats:
             divTotal = totalOnline.find_all("div")
@@ -31,35 +37,53 @@ class Stats():
                 total = list(totalOnline.text)
                 total[0:5] = [''.join(total[0:5])]
                 total[1:19] = [''.join(total[1:19])]
-                self.number = int(total[0])
-                self.name = str(total[1])
-                self.data_entry()
+                number = int(total[0])
+                name = str(total[1])
+                self.data_entry(name,number, dates)
 
 
-            
-  
+    def _create_table(self):
+        cursor = self.conn.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS tibiaDB(name TEXT, number REAL, time BLOB, date INTEGER)')
+
+    def data_entry(self,name,number, dates):
+        with sqlite3.connect('test2.db') as db:
+            cursor = db.cursor()
+            cursor.execute("INSERT INTO tibiaDB(name, number, time, date) VALUES(?, ?, ?, ?)",
+                           (name, number, time.strftime("%H:%M:%S"), dates))
+            self.conn.commit()
 
 
 
-    def create_table(self):
-        self.c.execute('CREATE TABLE IF NOT EXISTS tibiaDB(name TEXT, number REAL, time BLOB, date INTEGER)')
 
-    def data_entry(self):
-        self.c.execute("INSERT INTO tibiaDB(name, number, time, date) VALUES(?, ?, ?, ?)",
-                       (self.name, self.number, time.strftime("%H:%M:%S"), self.dates))
-        self.conn.commit()
+
+
+
+class GuiWindow():
+    def __init__(self):
+        root = Tk()
+        frames = Frame(root)
+        frames.pack()
+        playersOnline = Button(frames, text="Players online list", fg="blue", command = self.graph_players)
+        changes = Button(frames, text="Changes in max and min values", fg="blue", command = self.comparison)
+        playersOnline.pack(side=LEFT)
+        changes.pack(side=LEFT)
+        root.mainloop()
 
     def comparison(self):
-        highestNumber = self.c.execute('SELECT number, date, time FROM tibiaDB ORDER BY number DESC LIMIT 1')
+        with sqlite3.connect('test2.db') as db:
+            cursor = db.cursor()
+        highestNumber = cursor.execute('SELECT number, date, time FROM tibiaDB ORDER BY number DESC LIMIT 1')
         for rows in highestNumber:
             datesHigh = rows[1]
             yearHigh, monthHigh, dayHigh = datesHigh.split('-' )
             date_datetime = datetime.datetime.strptime(datesHigh, '%Y-%m-%d')
             int_datesHigh = date2num( date_datetime)
+            print(type(int_datesHigh))
             high = float(rows[0])
             print("The highest numbers of players online was " + str(rows[0]))
             
-        lowestNumber = self.c.execute('SELECT number,date, time FROM tibiaDB ORDER BY number ASC LIMIT 1')
+        lowestNumber = cursor.execute('SELECT number,date, time FROM tibiaDB ORDER BY number ASC LIMIT 1')
         for rows in lowestNumber:
             datesLow = rows[1]
             yearLow, monthLow, dayLow = datesLow.split('-' )
@@ -74,6 +98,15 @@ class Stats():
 
         percent = round((((low/high) * 100) - 100) *-1, 2)
 
+
+        if int_datesLow == int_datesHigh: 
+            percentChanges = "The Number of Players has raised " + str(percent) + "%"
+        elif int_datesLow == int_datesHigh: 
+            percentChanges = "The number of players has been reduced " + str(percent) + "%"
+        elif int_datesLow > int_datesHigh:
+            percentChanges = "The number of players has been reduced " + str(percent) + "%"
+        elif int_datesLow < int_datesHigh:
+            percentChanges = "The Number of Players has raised " + str(percent) + "%"
 
         if int_datesLow == int_datesHigh: 
             percentChanges = "The Number of Players has raised " + str(percent) + "%"
@@ -123,12 +156,12 @@ class Stats():
         fig.autofmt_xdate()
         plt.legend()
         plt.show()
-        
-            
 
 
-    def graphPlayers(self):
-        numberPlayer = self.c.execute('SELECT number FROM tibiaDB')
+    def graph_players(self):
+        with sqlite3.connect('test2.db') as db:
+            cursor = db.cursor()
+        numberPlayer = cursor.execute('SELECT number FROM tibiaDB')
         values = []
         for row in numberPlayer:
             floats = float(''.join(map(str,row)))
@@ -137,7 +170,7 @@ class Stats():
 
         
 
-        datePlayer = self.c.execute('SELECT date FROM tibiaDB')
+        datePlayer = cursor.execute('SELECT date FROM tibiaDB')
         values2 = []
         i = 0
         for row2 in datePlayer:
@@ -145,11 +178,6 @@ class Stats():
             date_datetime = datetime.datetime.strptime(floats2, '%Y-%m-%d')
             allDates = date2num( date_datetime)
             values2.append(allDates)
-
-
-        print(len(values2))
-         
-
 
         fig, ax = plt.subplots() 
         ax.plot(values2,values, label="Minimum number of players", color="red")
@@ -162,38 +190,5 @@ class Stats():
         fig.autofmt_xdate()
         plt.show()
 
-        self.c.close()
-        self.conn.close()
 
-class tkinter():
-    def __init__(self):
-        root = Tk()
-        frames = Frame(root)
-        frames.pack()
-        playersOnline = Button(frames, text="Players online list", fg="blue", command = self.executePlayers)
-        changes = Button(frames, text="Changes in max and min values", fg="blue", command = self.executeComparison)
-        playersOnline.pack(side=LEFT)
-        changes.pack(side=LEFT)
-        root.mainloop()
-
-
-    def executeComparison(self):
-        Stats().comparison()
-
-    def executePlayers(self):
-        Stats().graphPlayers()
-
-
-
-        
-        
-
-        
-    
-
-
-
-tkinter()
-Stats()
-
-
+GuiWindow()
